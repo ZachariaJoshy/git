88c349a3b9c2548237d8a13f875f75d842736b80
ZACHARIA JOSHY <22316@fisat>
2018-04-18 01:06:12 -0400

    create a python file to add 2 numbers and 3 number

--- add.py
+++ add.py
@@ -1,5 +1,9 @@
 
-a=int(raw_input("Enter num:"))
-b=int(raw_input("Enter num:"))
-c=a+b
-print c
+def add(a,b):
+   c=a+b
+   return c
+print add(2,3)
+def add1(a,b,c):
+   d=a+b+c
+   return d
+print add1(1,2,3)
